04671dfe0092283884bc6679717eea45d5b3196f
Christian Brown <chrisadbr@protonmail.com>
2020-05-26 13:08:41 +0300

    FYE 2019

--- anonymousBrowsing/anonymous_browsing.php
+++ anonymousBrowsing/anonymous_browsing.php
@@ -1,24 +1,26 @@
 <?php
-    require_once '../db_connect.php';
-    if (isset($_POST['btnUpload'])){
+require_once '../db_connect.php';
+if (isset($_POST['btnUpload'])) {
 
-        if(empty($_POST['topic'])){
-            ?>
-            <script>window.alert("Please enter topics title");</script>
-            <?php
-        }else{
-            $tittle = $_POST['tittle'];
-            $topic_tittle = $_POST['topic'];
-            $file = $_FILES['fileName']['name'];
-            move_uploaded_file($_FILES['fileName']['tmp_name'], $file);
+    if (empty($_POST['topic'])) {
+        ?>
+        <script>
+            window.alert("Please enter topics title");
+        </script>
+<?php
+    } else {
+        $tittle = $_POST['tittle'];
+        $topic_tittle = $_POST['topic'];
+        $file = $_FILES['fileName']['name'];
+        move_uploaded_file($_FILES['fileName']['tmp_name'], $file);
 
-            $insert_query = $conn->query("INSERT INTO anonymousBrowsing(tittle, topic, doc)
+        $insert_query = $conn->query("INSERT INTO anonymousBrowsing(tittle, topic, doc)
             VALUES('$tittle', '$topic_tittle', '$file')");
-                if($insert_query == true){
-                    header('Location: ../admin_page.php');
-                }else{
-                    echo "File not added";
-                }
+        if ($insert_query == true) {
+            header('Location: ../admin_page.php');
+        } else {
+            echo "File not added";
         }
     }
+}
 ?>

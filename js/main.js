4ecbd4db9f43ab6ddbe70e95e8fd7d21b6e4a72e
Christian Brown <chrisadbr@protonmail.com>
2020-05-27 18:29:58 +0300

    Made changes to a JS folder

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function mainSearch(data){
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
-        if(xhr.status == 200 && xhr.readyState){
+        if(xhr.status == 200 && xhr.readyState == 4){
             document.getElementById("search_result").innerHTML = this.responseText;
         }
     };
